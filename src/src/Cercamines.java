756ef639e6ef3e6c423017f5fd64de9d83747d62
AndreuPC <Apuigserver16104@alumnes.iesmanacor.cat>
2023-06-08 11:37:33 +0200

    Crear tauler

--- src/src/Cercamines.java
+++ src/src/Cercamines.java
@@ -1,8 +1,16 @@
-public class Cercamines {
-    public static void main(String[] args) {
+import java.util.Scanner;
+        public class Cercamines {
+            public static void main(String[] args) {
+                int rows = 8; // nombre de files
+                int cols = 8; // nombre de columnes
+                int numMines = 10; // nombre de mines
+
+            }
+        }
 
 
 
 
-    }
-}
+
+
+
